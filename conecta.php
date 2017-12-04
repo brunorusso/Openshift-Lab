806b89fbc3ea26c2e96bd172bdca72b2239471f0
Bruno Russo <bruno.russo@locaweb.com.br>
2017-12-04 10:04:32 -0200

    teste

--- conecta.php
+++ conecta.php
@@ -1,11 +1,11 @@
 <?php
 #Define variaveis para conexao ao banco de dados
 
-define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
-define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
-define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
-define('DB_PASSWORD', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
-define('DB_DATABASE', getenv('OPENSHIFT_GEAR_NAME'));
+define('DB_HOST', getenv('MYSQL_PORT_3306_TCP_ADDR'));
+define('DB_PORT', getenv('MYSQL_PORT_3306_TCP_PORT='));
+define('DB_USER', getenv('DB_USER'));
+define('DB_PASSWORD', getenv('DB_PASS'));
+define('DB_DATABASE', getenv('DB_NAME'));
 
 $dbhost = constant("DB_HOST"); // Host name 
 $dbport = constant("DB_PORT"); // Host port
@@ -14,7 +14,6 @@ $dbpassword = constant("DB_PASSWORD"); // MySQL password
 $db_name = constant("DB_DATABASE"); // Database name 
 
 $a = "dsdsds";
-
 
 
 echo "$dbhost - $dbport -  $dbusername -  $dbpassword - $db_name"
