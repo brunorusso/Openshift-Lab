97c7cfd8861313277614a48a369845b8f89644f4
Bruno Tadeu Russo <brusso@gmail.com>
2017-12-06 09:50:44 -0200

    Update stress.php

--- stress.php
+++ stress.php
@@ -1,23 +1,103 @@
-<?php
+<!DOCTYPE html>
+<html class="ls-theme-orange">
+ <head>
+    <title>Laboratório de Openshift com Locaweb STYLE - 26/09/2017</title>
 
-$min = 1;
-$max = 7;
-$num = rand($min,$max);
-$calculo = $num/2;
+    <meta charset="utf-8">
+    <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <meta name="description" content="Insira aqui a descrição da página.">
+    <link href="http://assets.locaweb.com.br/locastyle/3.10.0/stylesheets/locastyle.css" rel="stylesheet" type="text/css">
+    <link rel="icon" sizes="192x192" href="/locawebstyle/assets/images/ico-boilerplate.png">
+    <link rel="apple-touch-icon" href="/locawebstyle/assets/images/ico-boilerplate.png">
+  </head>
+  <body>
+    <div class="ls-topbar ">
 
-if ($calculo <= 1) {
-        print ("Analisando...");
-        $i=0;
-        while ($i<=9999999999999) {
-                $i++;
-                $j=0;
-                while ($j<=1000) {
-                        $j++;
+  <!-- Barra de Notificações -->
+  <div class="ls-notification-topbar">
+
+    <!-- Dropdown com detalhes da conta de usuário -->
+    <div data-ls-module="dropdown" class="ls-dropdown ls-user-account">
+      <a href="#" class="ls-ico-user">
+        <img src="/locawebstyle/assets/images/locastyle/avatar-example.jpg" alt="" />
+        <span class="ls-name">Usuário</span>
+        (johnkennedy)
+      </a>
+
+      <nav class="ls-dropdown-nav ls-user-menu">
+        <ul>
+          <li><a href="#">Sair</a></li>
+         </ul>
+      </nav>
+    </div>
+  </div>
+
+  <span class="ls-show-sidebar ls-ico-menu"></span>
+
+  
+  <!-- Nome do produto/marca com sidebar -->
+    <h1 class="ls-brand-name">
+      <a href="index.php" class="ls-ico-earth">
+        <small>Laboratório</small>
+        Openshift by Locaweb CORP
+      </a>
+    </h1>
+
+  <!-- Nome do produto/marca sem sidebar quando for o pre-painel  -->
+</div>
+
+
+    <aside class="ls-sidebar">
+      <nav class="ls-menu">
+        <ul>
+           <li><a href="dashboard.php" class="ls-ico-dashboard" title="Dashboard">Dashboard</a></li>
+           <li><a href="sobre.php" class="ls-ico-info" title="Sobre">Sobre</a></li>
+       </ul>
+      </nav>
+  </div>
+</aside>
+
+
+    <main class="ls-main ">
+      <div class="container-fluid">
+        <h1 class="ls-title-intro ls-ico-home">Página inicial</h1>
+        <h5 class="ls-title-3">Laboratório de Openshift</h5>
+        <div class="ls-box">
+                <?php
+
+                $min = 1;
+                $max = 7;
+                $num = rand($min,$max);
+                $calculo = $num/2;
+
+                if ($calculo <= 1) {
+                        print ("<p>Analisando...</p>");
+                        $i=0;
+                        while ($i<=9999999999999) {
+                                $i++;
+                                $j=0;
+                                while ($j<=1000) {
+                                        $j++;
+                                }
+                        }
+                } else {
+                        print("<p>Numero analisado: $num</p>");
                 }
-        }
-} else {
-        print("Numero analisado: $num");
-}
 
-?>
+                ?>
 
+
+  		
+  		
+
+        </div>
+        
+      </div>
+    </main>
+
+    <!-- We recommended use jQuery 1.10 or up -->
+    <script type="text/javascript" src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
+    <script src="http://assets.locaweb.com.br/locastyle/3.10.0/javascripts/locastyle.js" type="text/javascript"></script>
+  </body>
+</html>
